bd526fa943ce93546bc8719723adcdf7bf23a2d7
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-06-06 20:09:28 -0400

    all

---
+++ test.js
@@ -0,0 +1,51 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const directionofTravel = prompt("Would you like to move \"right\", \"left\",\"up\"or\"down\"?")
+
+
+
+const directionRight = "right"
+const directionLeft = "left"
+const directionUp = "up"
+const directionDown = "down"
+
+const stopProgram = "done"
+
+ let x = 0
+ 
+ let y = 0
+ 
+
+
+
+
+
+while (directionofTravel !== "done"){
+   
+    prompt("Would you like to move \"right\", \"left\",\"done\"\"up\"or\"down\"?");
+
+    if (directionofTravel == 'up'){
+        x=x+1 
+        console.log(x,y)
+        prompt("Would you like to move \"right\", \"left\",\"done\"\"up\"or\"down\"?");
+    }   
+    else if (directionofTravel === 'down'){
+        x=x-1
+        console.log(x,y)
+        prompt("Would you like to move \"right\", \"left\",\"done\"\"up\"or\"down\"?");
+    }    
+    else if (directionofTravel === "right"){
+        y=y+1
+        console.log (x,y)
+        prompt("Would you like to move \"right\", \"left\",\"done\"\"up\"or\"down\"?");
+    }
+    else if (directionofTravel === "left"){
+        y=y-1
+        console.log (x,y)
+        prompt("Would you like to move \"right\", \"left\",\"done\"\"up\"or\"down\"?");
+    }
+    else if (directionofTravel === "done"){
+        console.log("Your final destination is", x,y)
+    } 
+    
+}
---
+++ text-explorer.js
@@ -0,0 +1,53 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+
+
+const directionofTravel = ("Would you like to move \"right\", \"left\",\"done\"\"up\"or\"down\"?")
+const directionRight = "right"
+const directionLeft = "left"
+const directionUp = "up"
+const directionDown = "down"
+
+const stopProgram = "done"
+
+ let x = 0
+ let y = 0
+
+
+while (directionofTravel !== "done"){
+    
+    if (directionofTravel == 'up'){
+        x=x+1 
+        console.log("Your current position is",x,y)
+        prompt("Would you like to move \"right\", \"left\",\"done\"\"up\"or\"down\"?");
+    }   
+    else if (directionofTravel == 'down'){
+        x=x-1
+        console.log("Your current position is",x,y)
+        prompt("Would you like to move \"right\", \"left\",\"done\"\"up\"or\"down\"?");
+    }    
+    else if (directionofTravel == "right"){
+        y=y+1
+        console.log("Your current position is",x,y)
+        prompt("Would you like to move \"right\", \"left\",\"done\"\"up\"or\"down\"?");
+    }
+    else if (directionofTravel == "left"){
+        y=y-1
+        console.log("Your current position is",x,y)
+        prompt("Would you like to move \"right\", \"left\",\"done\"\"up\"or\"down\"?");
+    }    
+    else if (directionofTravel == "done"){
+        console.log("Your final destination is", x,y)
+    }    
+    
+
+}
+        
+
+
+    //else if ("down"){
+       // x=Number(down);
+       // x=x-1;
+       /// console.log(x,)
+       // prompt("Would you like to move \"right\", \"left\",\"up\"or\"down\";type \"done\" when finished playing?")
+
